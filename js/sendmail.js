cca398fd2b03c7806f20417a991e14f839b91b14
Enzo <enzo.gerola@fatec.sp.gov.br>
2020-02-21 08:59:06 -0300

    EG - Master Correção em Sendmail.js

--- js/sendmail.js
+++ js/sendmail.js
@@ -3,26 +3,28 @@ const sendmail = (() => {
   var client_email = document.getElementById('client_email').value;
   var subject = document.getElementById('subj').value;
   var msg = document.getElementById('msg').value;
-  let hostname = window.location.hostname;
-  let pathname = window.location.pathname;
 
   try {
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "enzop.gerola@gmail.com",
-        Password : "@EnzoFatec990099",
-        To : 'enzop.gerola@gmail.com',
-        From : `${client_email}`,
-        Subject : `${subject}`,
-        Body : `${msg}`
-    }).then((message) => {
-      showToast();
-      console.log(message);
-      window.location.href = `https://${hostname}${pathname}#contact-section`;
-    });
+    if(client_name != '' && client_email != '' && subject != '' && msg != ''){
+      Email.send({
+          Host : "smtp.gmail.com",
+          Username : "enzop.gerola@gmail.com",
+          Password : "@EnzoFatec990099",
+          To : 'enzop.gerola@gmail.com',
+          From : `${client_email}`,
+          Subject : `${subject}`,
+          Body : `Nome do Cliente: ${client_name} \n ${msg}`
+      }).then((message) => {
+        showToast();
+        console.log(message);
+        window.location.href = '#contact-section';
+      });
+    }else{
+      alert('Preencha todos os dados!');
+    }
   } catch (e) {
     showToastErr();
     console.log('Erro ao enviar email: ', e);
-    window.location.href = `https://${hostname}${pathname}#contact-section`;
+    window.location.href = '#contact-section';
   }
 });
