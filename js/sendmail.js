b1c16f1d5055f38bb679ec66375ce3c503e3a953
EnzoGerola <enzo.gerola@fatec.sp.gov.br>
2020-02-20 21:30:34 -0300

    EG - Master Adicionando funcionalidade de envio de email para contato (SMTP)

---
+++ js/sendmail.js
@@ -0,0 +1,24 @@
+const sendmail = (() => {
+  var client_name = document.getElementById('client_name').value;
+  var client_email = document.getElementById('client_email').value;
+  var subject = document.getElementById('subj').value;
+  var msg = document.getElementById('msg').value;
+
+  try {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "enzop.gerola@gmail.com",
+        Password : "@EnzoFatec990099",
+        To : 'enzop.gerola@gmail.com',
+        From : `${client_email}`,
+        Subject : `${subject}`,
+        Body : `${msg}`
+    }).then((message) => {
+      console.log(message);
+      showToast();
+    });
+  } catch (e) {
+    console.log('Erro ao enviar email: ', e);
+    showToastErr();
+  }
+});
